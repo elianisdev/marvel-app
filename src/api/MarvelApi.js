0bd4063cbc070dbd1ce70667ecbcb8030c30fd82
Eliana Suancha <elianitasuanguz@gmail.com>
2022-03-12 01:09:16 -0500

    * Avances en grid de personajes * Se crea filtro de busqueda y paginacion * Ajustes de scss * Se creo el archivo .env para variables de entorno

--- src/api/MarvelApi.js
+++ src/api/MarvelApi.js
@@ -1,6 +1,71 @@
-const API_KEY = import.meta.env.VITE_API_KEY;
-const ts = new Date().getTime();
+import md5 from "md5";
 
-// crear el hash de la petición
-const hash = md5(API_KEY + ts + API_KEY);
-console.log(API_KEY);
+const PRIVATE_API_KEY = import.meta.env.VITE_PRIVATE_API_KEY;
+const PUBLIC_API_KEY = import.meta.env.VITE_PUBLIC_API_KEY;
+const API_URL = import.meta.env.VITE_API_URL;
+const timeStamp = new Date().getTime();
+const Hash = md5(`${timeStamp}${PRIVATE_API_KEY}${PUBLIC_API_KEY}`);
+
+const request = async ( path, params = {}, method = 'GET' ) => {
+  params = {
+    apikey: PUBLIC_API_KEY,
+    ts: timeStamp,
+    hash: Hash,
+    ...params
+  };
+
+  let options = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+  };
+  let url = `${API_URL}/${path}`;
+  if ( method === 'GET' ) {
+    url += `?${new URLSearchParams( params )}`;
+  } else {
+    options.body = JSON.stringify( params );
+  }
+
+  return (await fetch(url, options)).json();
+};
+
+const MarvelApi = {
+  getCharacters: async (params) => {
+    return await request( 'characters', params );
+  },
+  getCharacter: async (id) => {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/characters/${id}?ts=${timeStamp}&apikey=${PUBLIC_API_KEY}&hash=${Hash}`
+    );
+      return await response.json();
+  },
+  getComics: async (id) => {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/characters/${id}/comics?ts=${timeStamp}&apikey=${PUBLIC_API_KEY}&hash=${Hash}`
+    );
+      return await response.json();
+  },
+  getComic: async (id) => {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/comics/${id}?ts=${timeStamp}&apikey=${PUBLIC_API_KEY}&hash=${Hash}`
+    );
+      return await response.json();
+  },
+  getCreators: async () => {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/creators?ts=${timeStamp}&apikey=${PUBLIC_API_KEY}&hash=${Hash}`
+    );
+      return await response.json();
+  },
+  getCreator: async (id) => {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/creators/${id}?ts=${timeStamp}&apikey=${PUBLIC_API_KEY}&hash=${Hash}`
+    );
+      return await response.json();
+  }
+};
+
+
+export default MarvelApi
--- src/views/characters/ViewCharacters.vue
+++ src/views/characters/ViewCharacters.vue
@@ -3,7 +3,8 @@ import GridCharters from "../../components/characters/GridCharters.vue";
 </script>
 <template>
 
-    <h2 class="fst-italic fw-bold">Characters</h2>
+    <h2 class="fst-italic fw-bold">Personajes</h2>
+    <hr>
     <div class="row py-4">
     <GridCharters />
     </div>
